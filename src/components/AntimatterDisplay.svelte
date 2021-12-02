3dc23130a60cc5e220a2ee2fff3da4cf1a95960e
jmroon <36086006+jmroon@users.noreply.github.com>
2021-12-01 22:26:16 -0500

    cool tailwind card

--- src/components/AntimatterDisplay.svelte
+++ src/components/AntimatterDisplay.svelte
@@ -2,6 +2,6 @@
   import { antimatter } from '@/stores/antimatter';
 </script>
 
-<div class="mx-auto text-center m-10 text-4xl font-semibold">
+<div class="mx-auto text-center m-10 text-4xl font-semibold select-none">
   Antimatter: {$antimatter.toFixed(0)}
 </div>
