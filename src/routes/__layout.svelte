a6899c6e848be4bde47e7beb96538fdd65930665
jmroon <36086006+jmroon@users.noreply.github.com>
2022-01-02 17:18:37 -0500

    more adjustments

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,11 +13,13 @@
   loadRoutes();
 </script>
 
-<nav class="flex items-center p-5 text-xl text-center text-gray-100 justify-between bg-slate-800 drop-shadow-md">
-  <a href="/">Dimensions</a>
-  <AntimatterDisplay />
-  <a href="/options">Options</a>
-</nav>
-<div class="h-full w-full bg-slate-50">
-  <slot />
+<div class="flex flex-col h-screen w-screen">
+  <nav class="flex items-center p-5 text-xl text-center text-gray-100 justify-between bg-slate-800 drop-shadow-md">
+    <a href="/">Dimensions</a>
+    <AntimatterDisplay />
+    <a href="/options">Options</a>
+  </nav>
+  <div class="w-full h-full overflow-x-hidden">
+    <slot />
+  </div>
 </div>
